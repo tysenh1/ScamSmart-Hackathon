6781c51c8a730761cb894440422b7c9aa09055d5
Tysen Heck <theck@academic.rrc.ca>
2025-02-21 16:47:48 -0600

    Added the images to the navbar

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -4,11 +4,11 @@ const Nav = () => {
   return (
     <div className='row-span-1 w-full bg-white drop-shadow-lg'>
         <div className='grid grid-cols-5 h-full'>
-          <button className='bg-slate-400'>learn</button>
-          <button className='bg-slate-300'>train</button>
-          <button className='bg-slate-400'>dashboard</button>
-          <button className='bg-slate-300'>confirm</button>
-          <button className='bg-slate-400'>counter</button>
+            <button className='bg-slate-400'><img src={"/navPictures/learn.png"} className={"w-auto h-auto m-auto"}></img></button>
+          <button className='bg-slate-300'><img src={"/navPictures/train.png"} className={"w-auto h-auto m-auto"}></img></button>
+          <button className='bg-slate-400'><img src={"/navPictures/dashboard.png"} className={"w-auto h-auto m-auto"}></img></button>
+          <button className='bg-slate-300'><img src={"/navPictures/confirm.png"} className={"w-auto h-auto m-auto"}></img></button>
+          <button className='bg-slate-400'><img src={"/navPictures/counter.png"} className={"w-auto h-auto m-auto"}></img></button>
         </div>
     </div>
   )
