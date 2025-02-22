0b38a07feef687da455713ddc56582056c1e9066
McCauley Armishaw <marmishaw@rrc.ca>
2025-02-21 18:03:26 -0600

    feat: Dashboard page started. Implemented cards for streak count display amd weak spots card which will direct to train page.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react';
+
+interface StreakCard {
+  streakCount: number,
+  streakDescription: string,
+  streakImage: string
+}
+
+interface WeakSpotsCard {
+  cardDescription: string
+}
+
+const StreakCardDisplay = () => {
+  const [streakCount] = useState(14);
+  
+  return (
+    <div className="flex items-center justify-center bg-white h-1/4 w-1/2 p-10 rounded-md">
+      <span className="text-3xl font-bold">Your Streak: </span>
+      <img
+       src="/dashboardImages/streakFireImage.svg"
+       alt="fireIcon"
+       className="w=9 h-9"
+      />
+      <span className="text-3xl font-bold">{streakCount} days!</span>
+    </div>
+  );
+};
+
+
+const WeakSpotsCardDisplay = () => {
+  const [cardDescription] = useState("Find your weak spots.");
+
+  return (
+    <div className="flex items-center justify-center bg-white w-1/3 h-1/0.75 py-4 px-10 my-6 p-5 rounded-md">
+      <span className="text-3xl font-bold">{cardDescription}</span>
+    </div>
+  );
+};
 
 const page = () => {
   return (
-    <div className="bg-green-500 h-full">this is the dashboad</div>
-  )
+    <div className="bg-green-500 h-full">
+      This is the dashboard
+    <StreakCardDisplay />
+    <WeakSpotsCardDisplay />
+    </div>
+  );
 }
 
-export default page
+
+export default page;
